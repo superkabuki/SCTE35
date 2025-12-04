f333ba03d421c5d4ce54518ec914e6c255a586d4
Adrian of Doom <a@iodisco.com>
2025-12-03 21:48:26 -0500

    added srtfu

--- setup.py
+++ setup.py
@@ -19,15 +19,15 @@ setuptools.setup(
     url="https://github.com/superkabuki/threefive",
     install_requires=[
         "pyaes",
+        "srtfu",
     ],
-    scripts=["bin/threefive","bin/threefive3",],
+    scripts=["bin/threefive,],
     packages=setuptools.find_packages(),
     classifiers=[
         "License :: OSI Approved :: Sleepycat License",
         "Environment :: Console",
         "Operating System :: OS Independent",
         "Operating System :: POSIX :: BSD :: OpenBSD",
-        "Operating System :: POSIX :: BSD :: NetBSD",
         "Operating System :: POSIX :: Linux",
         "Topic :: Multimedia :: Video",
         "Programming Language :: Python :: 3.8",
